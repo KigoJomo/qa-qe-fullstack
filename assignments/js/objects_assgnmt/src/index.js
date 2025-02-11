8cc1d6ff14bf9b184e896285b0b5af769bf82df6
KigoJomo <kigojomo@gmail.com>
2025-02-11 16:06:18 +0300

    Add product fetching functionality and HTML structure for display

--- assignments/js/objects_assgnmt/src/index.js
+++ assignments/js/objects_assgnmt/src/index.js
@@ -1,5 +1,23 @@
-const display = "45*2"
+const productsContainer = document.querySelector(".products-container")
 
-const result = eval(display)
+async function getData(){
+  try{
+    const response = await fetch('http://localhost:3000');
+    const products = await response.json()
 
-console.log(result)
+    for(const product of products.products){
+      console.log(product)
+      const item = document.createElement("div");
+      item.innerHTML = `
+        <h2>${product.name}</h2>
+        <p>${product.description}</p>
+        <p>${product.price}</p>
+        <p>${product.category}</p>
+        <p>${product.stock}</p>
+      `
+      productsContainer.appendChild(item)
+    };
+  } catch(error){
+    console.log('There was an error fetching prodcts:', error)
+  }
+}
